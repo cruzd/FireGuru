a34fd46878a99f490caa9c6481b195a43caa3c51
David Cruz <david.cruz@webdisplay.pt>
2018-08-26 10:36:49 +0100

    Changed the run.py to accomodate new data prep mode

--- run.py
+++ run.py
@@ -6,13 +6,18 @@ def main(_):
     parser = argparse.ArgumentParser(description='Process arguments.')
     parser.add_argument('-a', help='Which action? (Train or Prepare Data)', 
         choices=['train', 'data'])
+    parser.add_argument('-m', help='Real data or pessimist?', 
+        choices=['real', 'pessimist'])
     args = parser.parse_args()
 
     if args.a == 'train':
         from training import guru_train as train
         train.run_training()
     if args.a == 'data':
-        from data_prep import process_data as data
+        if args.m == 'pessimist':
+            from data_prep import pessimist_data as data
+        if args.m == 'real':
+            from data_prep import real_data as data
         data.run_data_process()
 
 if __name__ == '__main__':
