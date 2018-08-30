8c332a920eca2d73b026abfda1423e58888ad5d9
David Cruz <david.cruz@webdisplay.pt>
2018-08-30 21:40:58 +0100

    Divided file locations on parameters

--- training/parameters.py
+++ training/parameters.py
@@ -4,4 +4,5 @@ batch_size = 1000
 learning_rate = 0.01
 logs_dir = '/data/FireGuru/training/logs/'
 max_steps = 15000000
-filename = '/data/FireGuru/data/fine/teste.txt'
+real_filename = '/data/FireGuru/data/fine/teste.txt'
+pessimist_filename = '/data/FireGuru/data/fine/teste.txt'
